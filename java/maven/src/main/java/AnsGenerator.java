7f774cfe2c42d2e021f3e2ae5ea61663ce2d752e
JacobKangJie <jiekang@thoughtworks.com>
2015-02-03 11:26:35 +0800

    add history restrict

--- java/maven/src/main/java/AnsGenerator.java
+++ java/maven/src/main/java/AnsGenerator.java
@@ -1,26 +1,35 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by jiekang on 2/2/15.
  */
 public class AnsGenerator {
 
+    private Random rd;
+    private static Set<String> historyRecord;
+
+    AnsGenerator(Random random){
+        this.rd = new Random();
+        this.historyRecord = new HashSet<String>();
+
+    }
+
+
+
     public String generator() {
-
-        Random rd = new Random();
         StringBuffer ans = new StringBuffer();
-
         while (ans.length()<4) {
 
             String temp =String.valueOf(rd.nextInt(10));
             if(ans.indexOf(temp) < 0){
-                ans = ans.append(String.valueOf(rd.nextInt(10)));
+                ans = ans.append(temp);
             }
         }
 
-        return String.valueOf(ans);
+        while(!historyRecord.contains(ans)){
+        }
+
+        return ans.toString();
     }
 
 }
--- java/maven/src/test/java/AnsGeneratorTest.java
+++ java/maven/src/test/java/AnsGeneratorTest.java
@@ -1,9 +1,14 @@
 import org.junit.Test;
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Fail.fail;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
 
 /**
  * Created by jiekang on 2/2/15.
@@ -12,7 +17,7 @@ public class AnsGeneratorTest {
 
     @Test
     public void Should_be_all_digital_by_generator(){
-        AnsGenerator ag = new AnsGenerator();
+        AnsGenerator ag = new AnsGenerator(new Random());
         String result = ag.generator();
         for (int i = 0; i < result.length(); i++) {
             try {
@@ -25,20 +30,37 @@ public class AnsGeneratorTest {
 
     @Test
     public void Should_be_4_digital_length_by_generator() {
-        AnsGenerator ag = new AnsGenerator();
+        AnsGenerator ag = new AnsGenerator(new Random());
         int length = ag.generator().length();
-        assertSame(length, 4);
+        assertThat(length).isEqualTo(4);
 
     }
 
     @Test
     public void Should_be_4_different_digital(){
-        AnsGenerator ag = new AnsGenerator();
-        Set<Character> setResult = new HashSet<Character>();
-        for (int i = 0; i < ag.generator().length(); i++) {
-            setResult.add(ag.generator().charAt(i));
+        AnsGenerator ag = new AnsGenerator(new Random());
+        Set<Character> reSet = new HashSet<Character>();
+        String result = ag.generator();
+        for (int i = 0; i < result.length(); i++) {
+            reSet.add(result.charAt(i));
         }
-        assertSame(setResult.size(),4);
+        assertThat(reSet.size()).isEqualTo(4);
     }
-    
+
+    @Test
+    public void Should_not_be_in_3_previous_history(){
+
+        Random random = mock(Random.class);
+        AnsGenerator ag = new AnsGenerator(random);
+        given(random.nextInt(10))
+                .willReturn(1, 2, 3, 4)
+                .willReturn(1, 2, 3, 4)
+                .willReturn(2, 3, 1, 5);
+        Set<String> reSet = new HashSet<String>();
+        reSet.add(ag.generator());
+        reSet.add(ag.generator());
+        reSet.add(ag.generator());
+        assertThat(reSet.size()).isEqualTo(3);
+
+    }
 }
